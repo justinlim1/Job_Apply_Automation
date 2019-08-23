f20e987a237861a2b905b2575ac9331ca4067037
Justin <justinlim8@gmail.com>
2019-08-22 19:25:56 -0700

    starting variables and imports

--- Job Apply.py
+++ Job Apply.py
@@ -1,11 +1,20 @@
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as soup
 import urllib.request as ur
 from selenium import webdriver
 import re
 import sys
 
+url = '' #url of the job search site
+uCl = ur.urlopen(url)
+page_html = uCl.read()
+uCl.close()
+page = soup(page_html, "html.parser") #parse html
+
+driver = webdriver.Chrome()
+
+
 blacklist_words = []
 whitelist_words = ["intern", "internship", "software"] #List of required words in job title
 
-site_url = '' #url of the job search site
+
