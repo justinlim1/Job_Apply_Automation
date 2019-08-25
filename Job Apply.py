b6224b63d223f4f1e73c6ebc773b8f30b077f4e9
Justin <justinlim8@gmail.com>
2019-08-24 23:03:54 -0700

    Find the listed jobs on a linkedin page and make a list of all the job ids

--- Job Apply.py
+++ Job Apply.py
@@ -5,13 +5,24 @@ from selenium import webdriver
 import re
 import sys
 
-url = '' #url of the job search site
+url = 'https://www.indeed.com/jobs?q=Summer&l=Campbell%2C%20CA&start=10&advn=8857166483495170&vjk=a32a93717d9cf9f3' #url of the job search site
 uCl = ur.urlopen(url)
 page_html = uCl.read()
 uCl.close()
 page = soup(page_html, "html.parser") #parse html
+job_id = []
 
-driver = webdriver.Chrome()
+
+
+jobListings = page.find("td",{"id": "resultsCol"})
+jobs = jobListings.findAll('div', {"class": "jobsearch-SerpJobCard unifiedRow row result"})
+for job in jobs:
+    job_id.append(job['id'])
+print(job_id)
+
+
+
+#driver = webdriver.Chrome()
 
 
 blacklist_words = []
