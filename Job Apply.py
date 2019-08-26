88be018c2785b2dcdc88a03bda69d266377fd1d1
Justin <justinlim8@gmail.com>
2019-08-25 22:45:40 -0700

    added list of hrefs and webdriver options

--- Job Apply.py
+++ Job Apply.py
@@ -2,21 +2,24 @@
 from bs4 import BeautifulSoup as soup
 import urllib.request as ur
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import re
 import sys
 
 
 class jobApply:
 
-    #https://www.indeed.com/jobs?q=software+internshipo&l=Campbell%2C+CA'
-    def __intit__(self, url):
-        url = url
-        job_id = []
-        driver = webdriver.Chrome()
-        blacklist_words = []
-        whitelist_words = ["intern", "internship", "software"]  # List of required words in job title
-
-
+    #https://www.indeed.com/jobs?q=software+internshipo&l=Campbell%2C+CA
+    def __init__(self, page_url):
+        self.options = Options()
+        self.options.add_argument("--headless")
+        self.url = page_url
+        self.job_id = []
+        self.href_list = []
+        self.driver = webdriver.Chrome(options=self.options)
+        self.blacklist_words = []
+        self.whitelist_words = ["intern", "internship", "software"]  # List of required words in job title
+        self.get_html()
 
     def get_html(self): #parse html from given url
         uCl = ur.urlopen(self.url)
@@ -25,7 +28,6 @@ class jobApply:
         self.page = soup(page_html, "html.parser")
         self.job_id = []
 
-
     def getJobList(self):
         jobListings = self.page.find("td",{"id": "resultsCol"})
         jobs = jobListings.findAll("div", {"class": "jobsearch-SerpJobCard unifiedRow row result"})
@@ -33,9 +35,19 @@ class jobApply:
         for job in jobs:
             if job.find("div", {"class":"iaWrapper"}):
                 self.job_id.append(job['id'])
-        #print(job_id)
+                for link in job.findAll("a", {"target":"_blank"}):
+                    self.href_list.append(link['href'])
+        print(self.href_list)
 
 
+
+    def apply(self):
+        self.getJobList()
+
+
+url ="https://www.indeed.com/q-software-internshipo-l-Campbell,-CA-jobs.html?advn=7983672015724620&vjk=d86b7d62ea5e6c88"
+bot = jobApply(url)
+bot.apply()
 
 
 
