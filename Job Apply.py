cc4f6f0fe3aa6fe67eade0685d506fbcfb01b937
Justin <justinlim8@gmail.com>
2019-09-04 16:01:51 -0700

    switched site to linkedin

--- Job Apply.py
+++ Job Apply.py
@@ -6,6 +6,10 @@ from selenium.webdriver.chrome.options import Options
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import re
 import sys
 
@@ -14,37 +18,51 @@ MAX_JOBS = 100
 
 class jobApply:
 
-    #https://www.indeed.com/jobs?q=software+internshipo&l=Campbell%2C+CA
+
     def __init__(self, page_url):
         self.options = Options()
         #self.options.add_argument("--headless")
         self.url = page_url
-        self.mainUrl = "http://indeed.com"
+        self.mainUrl = "http://linkedin.com"
         self.job_id = [] #list of job ids
         self.job_urls = [] #list of job urls
         self.driver = webdriver.Chrome(options=self.options)
         self.blacklist_words = []
         self.whitelist_words = ["intern", "internship", "software"]  # List of required words in job title
-        self.get_html()
-        self. resumePath = ""
+        self.resumePath = ""
 
-    def get_html(self): #parse html from given url
-        uCl = ur.urlopen(self.url)
-        page_html = uCl.read()
-        uCl.close()
-        self.page = soup(page_html, "html.parser")
-        self.job_id = []
+
+    def find_page(self, url):
+        self.driver.get(url)
+        self.driver.find_element_by_xpath("//button[@aria-controls='linkedin-features-facet-values']").click()
+        self.driver.find_element_by_xpath("//label[@for='f_LF-f_AL']").click()
+        self.driver.find_element_by_tag_name('body').send_keys(Keys.RETURN)
+        sleep(1)
+        self.jobResults = self.driver.find_element_by_xpath("//div[@class='t-12 t-black--light t-normal']").text
+
+        print(self.jobResults)
+
+
 
     def getJobList(self):
-        jobListings = self.page.find("td",{"id": "resultsCol"})
-        jobs = jobListings.findAll("div", {"class": "jobsearch-SerpJobCard unifiedRow row result"})
+        self.find_page("https://www.linkedin.com/jobs/search/?keywords=software%20internship")
 
-        for job in jobs:
-            if job.find("div", {"class":"iaWrapper"}):
-                self.job_id.append(job['id']) #append job id
-                for link in job.findAll("a", {"target":"_blank"}):
-                    self.job_urls.append(self.mainUrl + link['href']) #append url of each job to list
-        #print(self.job_urls)
+        #while len(self.job_urls) < MAX_JOBS:
+        sleep(1)
+        jobListings = self.driver.find_elements_by_xpath("//div[@data-job-id]")
+
+
+        print(len(jobListings))
+
+        for job in jobListings:
+
+            self.job_id.append(job.get_attribute('data-job-id').split(":")[3]) #append job id
+            jobLink = job.find_element_by_xpath('//a[@data-control-id]')
+
+            self.job_urls.append(jobLink.get_attribute("href")) #append url of each job to list
+        print(len(self.job_urls))
+        print(self.job_id)
+
 
     def jobPage(self, url):
 
@@ -69,21 +87,23 @@ class jobApply:
         Tk().withdraw()
         self.resumePath = askopenfilename()
         print(self.resumePath)
+
     def login(self,email,password):
-        self.driver.get('https://secure.indeed.com/account/login?hl=en_US&co=US&continue=https%3A%2F%2Fwww.indeed.com%2F&tmpl=desktop&service=&from=gnav-util-homepage&_ga=2.210805846.21291967.1566709415-1824541224.1566527853')
-        self.driver.find_element_by_name("__email").send_keys(email)
+        self.driver.get('https://www.linkedin.com/login?trk=guest_homepage-basic_nav-header-signin')
+        self.driver.find_element_by_id("username").send_keys(email)
         sleep(1)
-        self.driver.find_element_by_name("__password").send_keys(password)
+        self.driver.find_element_by_id("password").send_keys(password)
         sleep(1)
-        self.driver.find_element_by_xpath("//button[@id='login-submit-button']").click()
+        self.driver.find_element_by_xpath("//button[@type='submit']").click()
         sleep(1)
+
 
 
     def apply(self):
-        self.login("justinlim8@gmail.com", "gzzc2g93")
-        #self.getJobList()
-        self.chooseResume()
-        self.jobPage("https://www.indeed.com/viewjob?jk=80075835c181f85b&q=software+internship&l=Campbell%2C+CA&tk=1dj8simpabqlc801&from=web&vjs=3")
+        self.login("justinlim8@gmail.com", "201404")
+        self.getJobList()
+        #self.chooseResume()
+        #self.jobPage("https://www.indeed.com/viewjob?jk=80075835c181f85b&q=software+internship&l=Campbell%2C+CA&tk=1dj8simpabqlc801&from=web&vjs=3")
 
 
     # def uploadResume(self):
@@ -92,7 +112,7 @@ class jobApply:
 
 
 
-url ="https://www.indeed.com/q-software-internshipo-l-Campbell,-CA-jobs.html?advn=7983672015724620&vjk=d86b7d62ea5e6c88"
+url = "https://www.linkedin.com/jobs/search/?f_LF=f_AL&keywords=software%20internship&location=San%20Jose%2C%20California%2C%20United%20States"
 bot = jobApply(url)
 bot.apply()
 
