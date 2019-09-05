8d59a43054adb9d6ba65c2e8f1ec898dc77d074f
Justin <justinlim8@gmail.com>
2019-09-04 19:52:30 -0700

    fixed finding jobs and retrieving job list/urls

--- Job Apply.py
+++ Job Apply.py
@@ -40,28 +40,35 @@ class jobApply:
         sleep(1)
         self.jobResults = self.driver.find_element_by_xpath("//div[@class='t-12 t-black--light t-normal']").text
 
-        print(self.jobResults)
+
 
 
 
     def getJobList(self):
+
         self.find_page("https://www.linkedin.com/jobs/search/?keywords=software%20internship")
+        #self.driver.set_window_position(0, 0)
+        #self.driver.maximize_window()
+        page = self.load_page()
 
         #while len(self.job_urls) < MAX_JOBS:
-        sleep(1)
-        jobListings = self.driver.find_elements_by_xpath("//div[@data-job-id]")
 
+        jobListings = page.find("ul", {"class": "jobs-search-results__list artdeco-list"})
 
-        print(len(jobListings))
+        jobs = jobListings.findAll("div", {"data-control-name": "A_jobssearch_job_result_click"})
+        print(jobs)
+        for job in jobs:
 
-        for job in jobListings:
+            self.job_id.append(job['data-job-id'].split(":")[3])  # append job id
+            self.job_urls.append("https://www.linkedin.com" + job.find("a", {"data-control-name":"A_jobssearch_job_result_click"})['href'])
+        # for job in jobListings:
+        #
+        #     self.job_id.append(job.get_attribute('data-job-id').split(":")[3]) #append job id
+        #     jobLink = job.find_element_by_xpath('//a[@data-control-id]')
+        #
+        #     self.job_urls.append(jobLink.get_attribute("href")) #append url of each job to list
 
-            self.job_id.append(job.get_attribute('data-job-id').split(":")[3]) #append job id
-            jobLink = job.find_element_by_xpath('//a[@data-control-id]')
-
-            self.job_urls.append(jobLink.get_attribute("href")) #append url of each job to list
-        print(len(self.job_urls))
-        print(self.job_id)
+        print(self.job_urls)
 
 
     def jobPage(self, url):
@@ -81,7 +88,16 @@ class jobApply:
         sleep(1)
         uploadFile.sendKeys(self.resumePath)
 
+    def load_page(self):
+        scroll_page = 0
+        while scroll_page < 50:
+            self.driver.find_element_by_xpath("//div[@class='jobs-search-results jobs-search-results--is-two-pane']").send_keys(Keys.PAGE_DOWN)
+            scroll_page += 1
 
+
+
+        page = soup(self.driver.page_source, "html.parser")
+        return page
 
     def chooseResume(self):
         Tk().withdraw()
@@ -104,6 +120,7 @@ class jobApply:
         self.getJobList()
         #self.chooseResume()
         #self.jobPage("https://www.indeed.com/viewjob?jk=80075835c181f85b&q=software+internship&l=Campbell%2C+CA&tk=1dj8simpabqlc801&from=web&vjs=3")
+
 
 
     # def uploadResume(self):
